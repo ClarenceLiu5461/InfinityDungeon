d26d4b78014e6eafab8da5236a5666f8b4bed65e
fenasot <79359861+fenasot@users.noreply.github.com>
2023-02-02 22:48:03 +0800

    TimebarPrefebCreate

---
+++ Assets/test/TimeCounter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    static int time_i = 60;
+    bool start_Timer3 = true;
+    public Image timeeeee ;
+    
+    
+
+    void Start()
+    {
+    }
+    
+    IEnumerator timer3()
+    {
+        yield return new WaitForSeconds(1f);
+        time_i--;
+        start_Timer3 = true;
+        
+    }
+
+    void Update()
+    {
+        if (start_Timer3)
+        {
+            StartCoroutine("timer3");
+            timeeeee = this.gameObject.GetComponent<Image>();
+            timeeeee.fillAmount = time_i / 60f;
+            start_Timer3 = false;
+            Debug.Log(time_i);
+        }
+    }
+    
+    
+   
+
+}
