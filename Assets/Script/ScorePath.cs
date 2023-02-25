3be3b02f3417e0e3271f9604a13f54c0187cced0
ClarenceLiu5461 <frank1213450@gmail.com>
2023-02-25 16:53:09 +0800

    Add Right and Wrong ways Sound Effects

--- Assets/Script/ScorePath.cs
+++ Assets/Script/ScorePath.cs
@@ -5,27 +5,28 @@ using UnityEngine;
 public class ScorePath : MonoBehaviour
 {
     int Score = 0;
+    public AudioClip RightWay;
+    public AudioClip WrongWay;
+    AudioSource audiosource;
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
-    public void GeneratePath()
-    {
-        
+        audiosource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.name == "Score")
+        if (collision.gameObject.tag == "Score")
         {
+            Debug.Log("Score Exists");
             Score++;
-            Destroy(this.gameObject);
+            audiosource.PlayOneShot(RightWay);
+            //Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Minus")
+        {
+            Debug.Log("Minus Exists");
+            audiosource.PlayOneShot(WrongWay);
+            //Destroy(collision.gameObject);
         }
     }
 }
